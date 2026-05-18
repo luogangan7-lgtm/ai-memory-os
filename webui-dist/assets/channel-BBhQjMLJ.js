2c15cb28ba25bdcecdfc2fdc692a5e696a3a0e70
luogangan7-lgtm <luogangan7@gmail.com>
2026-05-18 10:25:15 +0800

    fix: group providers by region with optgroup (🇨🇳中国/🌐海外)

--- webui-dist/assets/channel-BBhQjMLJ.js
+++
@@ -1,2 +0,0 @@
-import{ai as o,aj as n}from"./mermaid.core-BvRIyIHo.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
-//# sourceMappingURL=channel-BBhQjMLJ.js.map
--- webui-dist/assets/stateDiagram-v2-BHNVJYJU-Iawh-cbO.js
+++
@@ -1,2 +0,0 @@
-import{s as e,b as r,a,S as s}from"./chunk-AQP2D5EJ-CdkC-1xn.js";import{_ as i}from"./mermaid.core-BvRIyIHo.js";import"./chunk-55IACEB6-DkFCKKzp.js";import"./chunk-2J33WTMH-B6LuH50H.js";import"./index-BrdWNQto.js";var p={parser:a,get db(){return new s(2)},renderer:r,styles:e,init:i(t=>{t.state||(t.state={}),t.state.arrowMarkerAbsolute=t.arrowMarkerAbsolute},"init")};export{p as diagram};
-//# sourceMappingURL=stateDiagram-v2-BHNVJYJU-Iawh-cbO.js.map
