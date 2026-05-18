22184d005e2d9c3330734e2ffe8daa69e8a62281
luogangan7-lgtm <luogangan7@gmail.com>
2026-05-18 10:23:32 +0800

    fix: LLMStatusBar now shows region flag 🇨🇳/🌐 for China/International providers

--- webui-dist/assets/chunk-QZHKN3VN-DKgn7X0X.js
+++
@@ -1,2 +0,0 @@
-import{_ as s}from"./mermaid.core-BjD_9Cp1.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as I};
-//# sourceMappingURL=chunk-QZHKN3VN-DKgn7X0X.js.map
